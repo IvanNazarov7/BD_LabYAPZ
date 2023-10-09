6b0f7bbd5d80ba0af642a76ad72672d7d0dc2fe2
Іван Назаров <vet.nazarov7@gmail.com>
2023-10-09 03:36:14 +0300

    Clean a Code

--- Код/Form1.cs
+++ Код/Form1.cs
@@ -1,15 +1,7 @@
 ﻿using System;
 using MySql.Data.MySqlClient;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using Org.BouncyCastle.Asn1.Cms;
-using MySqlX.XDevAPI.Relational;
 
 namespace Код
 {
